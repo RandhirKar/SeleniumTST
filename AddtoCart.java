e3ab32c8a531539029bb41dff6ee15e38f16d2d2
RandhirKar <106177178+RandhirKar@users.noreply.github.com>
2022-09-13 08:30:06 +0545

    Dress

--- AddtoCart.java
+++ AddtoCart.java
@@ -1,4 +1,6 @@
-package TESTUI;
+package Dresses;
+
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -7,77 +9,88 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AddtoCart {
 
-	public static void main(String[] args) throws InterruptedException {
-		
-		
-		System.setProperty("webdriver.chrome.driver","C:\\Browserdriver\\chromedriver.exe");
-		
-		ChromeDriver driver = new ChromeDriver();
-		driver.get("https://www.saucedemo.com/");
-		driver.manage().window().maximize();
-		JavascriptExecutor js = (JavascriptExecutor)driver;
-		
-		WebElement EmailBox1 = driver.findElement(By.id("user-name"));
-		EmailBox1.sendKeys("standard_user");
-		Thread.sleep(2000);
-		WebElement PaswordBox = driver.findElement(By.id("password"));
-		PaswordBox.sendKeys("secret_sauce");
-		Thread.sleep(2000);
-		WebElement nextBttn = driver.findElement(By.id("login-button"));
-		nextBttn.click();
-		Thread.sleep(2000);
-		
-		WebElement scrollBtn = driver.findElement(By.className("product_sort_container"));
-		scrollBtn.click();
-		Thread.sleep(2000);
-		WebElement sortBtn = driver.findElement(By.xpath("//*[@id=\"header_container\"]/div[2]/div[2]/span/select/option[2]"));
-		sortBtn.click();
-		Thread.sleep(2000);
-		js.executeScript("window.scrollBy(0,4000)");
-		Thread.sleep(2000);
-		WebElement AddcartBtn = driver.findElement(By.name("add-to-cart-sauce-labs-backpack"));
-		AddcartBtn.click();
-		Thread.sleep(2000);
-		js.executeScript("window.scrollBy(0,-4000)");
-		Thread.sleep(2000);
-		WebElement AddcartBtn1 = driver.findElement(By.name("add-to-cart-test.allthethings()-t-shirt-(red)"));
-		AddcartBtn1.click();
-		Thread.sleep(2000);
-		WebElement cartBtn = driver.findElement(By.className("shopping_cart_link"));
-		cartBtn.click();
-		Thread.sleep(2000);
-		WebElement ChkBtn = driver.findElement(By.name("checkout"));
-		ChkBtn.click();
-		Thread.sleep(2000);
-		WebElement Fname = driver.findElement(By.id("first-name"));
-		Fname.sendKeys("Randhir");
-		Thread.sleep(2000);
-		WebElement Lname = driver.findElement(By.id("last-name"));
-		Lname.sendKeys("Karn");
-		Thread.sleep(2000);
-		WebElement Zipcod = driver.findElement(By.id("postal-code"));
-		Zipcod.sendKeys("12345");
-		Thread.sleep(2000);
-		WebElement contiBtn = driver.findElement(By.id("continue"));
-		contiBtn.click();
-		
-		Thread.sleep(2000);
-		js.executeScript("window.scrollBy(0,4000)");
-		Thread.sleep(2000);
-		WebElement finBtn = driver.findElement(By.id("finish"));
-		finBtn.click();
-		Thread.sleep(2000);
-		WebElement BkBtn = driver.findElement(By.id("back-to-products"));
-		BkBtn.click();
-		
-		
-		
-		
-		
-		
-		
-		
-
+		@SuppressWarnings("deprecation")
+		public static void main(String[] args) throws InterruptedException {
+			
+			
+			System.setProperty("webdriver.chrome.driver","C:\\Browserdriver\\chromedriver.exe");
+			  
+			ChromeDriver driver = new ChromeDriver();
+			driver.get("https://automationpractice.com/");
+			driver.manage().window().maximize();
+			JavascriptExecutor js = (JavascriptExecutor)driver;
+			Thread.sleep(2000);
+			
+			// Login
+			
+			WebElement SignBtn = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[1]/a"));
+			SignBtn.click();
+			Thread.sleep(2000);
+			js.executeScript("window.scrollBy(0,300)");
+					
+			WebElement EmlBtn = driver.findElement(By.id("email"));
+			EmlBtn.click();
+			Thread.sleep(1000);
+			EmlBtn.sendKeys("randhirall20@gmail.com");
+			Thread.sleep(1000);
+						
+			WebElement PwdBtn = driver.findElement(By.id("passwd"));
+			PwdBtn.click();
+			PwdBtn.sendKeys("automation");
+			Thread.sleep(1000);
+						
+			WebElement SignBttn = driver.findElement(By.id("SubmitLogin"));
+			SignBttn.click();
+			Thread.sleep(1000);
+			
+			//Search Functionality
+			
+			WebElement Scr = driver.findElement(By.id("search_query_top"));
+			Scr.click();
+			Scr.sendKeys("T-Shirts");
+			Thread.sleep(1000);
+			WebElement ScrBtn = driver.findElement(By.xpath("//*[@id=\"searchbox\"]/button"));
+			ScrBtn.click();
+			Thread.sleep(1000);
+			js.executeScript("window.scrollBy(0,300)");
+			WebElement Blouse = driver.findElement(By.xpath("//*[@id=\"best-sellers_block_right\"]/div/ul/li[3]/div/h5/a"));
+			Blouse.click();
+			js.executeScript("window.scrollBy(0,300)");
+			
+			//Selecting Item
+			driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS) ;
+			WebElement Qty = driver.findElement(By.xpath("//*[@id=\"quantity_wanted_p\"]/a[2]/span/i"));
+			Qty.click();
+			Thread.sleep(1000);
+			WebElement Sz = driver.findElement(By.id("group_1"));
+			Sz.click();
+			Thread.sleep(1000);
+			WebElement SltSz = driver.findElement(By.xpath("//*[@id=\"group_1\"]/option[3]"));
+			SltSz.click();
+			Thread.sleep(1000);
+			WebElement AddCt = driver.findElement(By.xpath("//*[@id=\"add_to_cart\"]/button"));
+			AddCt.click();
+			Thread.sleep(1000);
+			WebElement ChkOut = driver.findElement(By.xpath("//*[@id=\"layer_cart\"]/div[1]/div[2]/div[4]/a"));
+			ChkOut.click();
+			Thread.sleep(1000);
+			WebElement ChkOut1 = driver.findElement(By.xpath("//*[@id=\"center_column\"]/p[2]/a[1]"));
+			ChkOut1.click();
+			Thread.sleep(1000);
+			WebElement ChkOut2 = driver.findElement(By.xpath("//*[@id=\"center_column\"]/form/p/button"));
+			ChkOut2.click();
+			WebElement CkBox = driver.findElement(By.id("cgv"));
+			CkBox.click();
+			Thread.sleep(1000);
+			WebElement ChkOut3 = driver.findElement(By.xpath("//*[@id=\"form\"]/p/button"));
+			ChkOut3.click();
+			Thread.sleep(1000);
+			WebElement Bnk = driver.findElement(By.xpath("//*[@id=\"HOOK_PAYMENT\"]/div[1]/div/p/a"));
+			Bnk.click();
+			Thread.sleep(1000);
+			WebElement Cfm = driver.findElement(By.xpath("//*[@id=\"cart_navigation\"]/button"));
+			Cfm.click();
+			js.executeScript("window.scrollBy(0,300)");
 	}
 
 }
--- AddtoCart1.java
+++ AddtoCart1.java
@@ -1,12 +1,17 @@
 package Dresses;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
-public class Logout {
+public class AddtoCart1 {
+	
 
+	@SuppressWarnings("deprecation")
 	public static void main(String[] args) throws InterruptedException {
 		
 		
@@ -24,32 +29,51 @@ public class Logout {
 		SignBtn.click();
 		Thread.sleep(2000);
 		js.executeScript("window.scrollBy(0,300)");
-		
+				
 		WebElement EmlBtn = driver.findElement(By.id("email"));
 		EmlBtn.click();
 		Thread.sleep(1000);
 		EmlBtn.sendKeys("randhirall20@gmail.com");
 		Thread.sleep(1000);
-		
+					
 		WebElement PwdBtn = driver.findElement(By.id("passwd"));
 		PwdBtn.click();
 		PwdBtn.sendKeys("automation");
 		Thread.sleep(1000);
-		
+					
 		WebElement SignBttn = driver.findElement(By.id("SubmitLogin"));
 		SignBttn.click();
+		Thread.sleep(1000);
 		
-		WebElement Img = driver.findElement(By.xpath("//*[@id=\"header_logo\"]/a/img"));
-		Img.click();
+		WebElement LdBtn = driver.findElement(By.xpath("//*[@id=\"header_logo\"]/a/img"));
+		LdBtn.click();
+		Thread.sleep(2000);
+		
+		WebElement DrssBtn = driver.findElement(By.xpath("//*[@id=\"block_top_menu\"]/ul/li[2]/a"));
+		DrssBtn.click();
+		Thread.sleep(2000);
+		
+		WebElement CslDrss = driver.findElement(By.xpath("//*[@id=\"subcategories\"]/ul/li[2]/h5/a"));
+		CslDrss.click();
+		Thread.sleep(2000);
+		js.executeScript("window.scrollBy(0,700)");
+		
+		WebElement Review = driver.findElement(By.xpath("//*[@id=\"center_column\"]/ul/li/div/div[1]/div/a[1]/img"));
+		Review.click();
+		Thread.sleep(2000);
 		
 		
-		WebElement LoutBtn = driver.findElement(By.xpath("//*[@id=\"header\"]/div[2]/div/div/nav/div[2]/a"));
-		LoutBtn.click();
+		WebElement Pink = driver.findElement(By.id("color_24"));
+		Pink.click();
+		Thread.sleep(2000);
+		
+		WebElement AdCrt = driver.findElement(By.xpath("//*[@id=\"add_to_cart\"]/button"));
+		AdCrt.click();
+		Thread.sleep(5000);
 		
 		
 		
-		
-
-	}
-
+	}	
 }
+	
+	
