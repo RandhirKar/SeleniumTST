f2caf649c91e5c958154bb2bb279e2df73393c9f
RandhirKar <106177178+RandhirKar@users.noreply.github.com>
2022-09-16 11:55:51 +0545

    TestNG

---
+++ DataProviderDemo.java
@@ -0,0 +1,49 @@
+package TEST_1;
+
+import java.lang.reflect.Method;
+import org.testng.annotations.DataProvider;
+
+public class DataProviderDemo {
+	
+	@DataProvider(name = "create")
+	public Object[][] dataSet1(Method m)
+	{
+		Object[][] testdata = null;		
+		
+		if(m.getName().equals("test"))
+		{
+		testdata = new Object[][]
+			{
+				{"username","password"},
+				{"username1","password1"},
+				{"username1","password2"},
+				{"username1","password3"}
+		};
+
+	}	
+		else if (m.getName().equals("test1"))
+		{
+			testdata = new Object[][]
+					{
+						{"username","password","third"},
+						{"username1","password1","third1"},
+						{"username1","password2","third2"},
+						{"username1","password3","third3 "}
+					};
+		}
+		
+		else if (m.getName().equals("test2"))
+		{
+			testdata = new Object[][]
+					{
+						{"username","password","third","fourth"},
+						{"username1","password1","third1","fourth1"},
+						{"username1","password2","third2","fourth2"},
+						{"username1","password3","third3","fourth3"}
+					};
+		}
+		return testdata;
+	} 
+	
+
+}
